aa31c0e5076cc51af185a2d0328e833aebd8ab2f
SerafimArts <nesk@xakep.ru>
2023-09-20 11:47:53 +0000

    Upgrade webonyx graphql to 15.0+

---
+++ src/EventDispatcherInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Railt\EventDispatcher;
+
+use Psr\EventDispatcher\EventDispatcherInterface as PsrEventDispatcherInterface;
+
+/**
+ * Allows providing hooks on domain-specific lifecycles by dispatching events.
+ */
+interface EventDispatcherInterface extends PsrEventDispatcherInterface, EventListenerInterface
+{
+    /**
+     * Dispatches an event to all registered listeners.
+     *
+     * @template TArgEvent of object
+     *
+     * @param TArgEvent $event The event to pass to the event handlers/listeners.
+     *
+     * @return TArgEvent The passed $event MUST be returned.
+     */
+    public function dispatch(object $event): object;
+}
